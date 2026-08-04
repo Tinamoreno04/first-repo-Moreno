f95369df95b513acea4519a4a0f7f25d7c3bc579
Tinamoreno04 <tinamore0408@gmail.com>
2026-08-03 18:12:19 -0600

    Agrego opcion de elegir porcentajde de propina

---
+++ tictac.py
@@ -0,0 +1,16 @@
+
+#representacion de board y casillas
+
+board =[["_","_","_",],
+        ["_","_","_",],
+        ["_","_","_",]]
+
+if board [0][0] == "X" and board [0][1] == [0][2]:
+    print("Gano x")
+
+if board [1][0] == "X" and board [2][1] == [2][2]:
+    print("Gano x")
+
+if board [3][0] == "X" and board [3][1] == [3][2]:
+    print("Gano x")
+        
